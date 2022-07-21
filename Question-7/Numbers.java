a8bbb31c9054dcbc4a0b06f2489432402821373d
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-21 17:58:20 +0530

    Update Numbers.java

--- Question-7/Numbers.java
+++ Question-7/Numbers.java
@@ -4,13 +4,13 @@ public class Numbers {
 	public static void main(String[] args) {
 		for(int i = 1; i<=100; i++) {
 			if(i%3==0) {
-				System.out.println(i + " - Deep");
+				System.out.println(i + " - Deep (Multiple of 3)");
 			}
 			if(i%5==0) {
-				System.out.println(i + " - Tech");
+				System.out.println(i + " - Tech (Multiple of 5)");
 			}
 			if(i%3==0 && i%5==0) {
-				System.out.println(i + " - DeepTech");
+				System.out.println(i + " - DeepTech (Multiple of 3 & 5)");
 			}
 			else {
 				System.out.println(i);
