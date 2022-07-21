841a37abac8164d8b014f9b7a64ea48f345d03f6
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-21 17:47:17 +0530

    Changed Numbers.java

--- Question-7/Numbers.java
+++ Question-7/Numbers.java
@@ -1,16 +1,21 @@
+
 public class Numbers {
 
 	public static void main(String[] args) {
 		for(int i = 1; i<=100; i++) {
 			if(i%3==0) {
-				System.out.println("Deep");
+				System.out.println(i + " - Deep");
 			}
 			if(i%5==0) {
-				System.out.println("Tech");
+				System.out.println(i + " - Tech");
 			}
-			if(i%2==0 && i%5==0) {
-				System.out.println("DeepTech");
+			if(i%3==0 && i%5==0) {
+				System.out.println(i + " - DeepTech");
 			}
+			else {
+				System.out.println(i);
+			}
+			
 		}
 
 	}
