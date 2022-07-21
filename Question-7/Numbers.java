66ecfd89637fcef033300b7fe2b7a87da176905b
Shubham Lohar <82263361+MrShubhamL@users.noreply.github.com>
2022-07-21 18:18:46 +0530

    Replace The Numbers

--- Question-7/Numbers.java
+++ Question-7/Numbers.java
@@ -1,21 +1,23 @@
-
-public class Numbers {
+class Numbers {
 
 	public static void main(String[] args) {
 		for(int i = 1; i<=100; i++) {
 			if(i%3==0) {
+				// First Condition
 				System.out.println(i + " - Deep (Multiple of 3)");
 			}
 			if(i%5==0) {
+				// Second Condition
 				System.out.println(i + " - Tech (Multiple of 5)");
 			}
 			if(i%3==0 && i%5==0) {
-				System.out.println(i + " - DeepTech (Multiple of 3 & 5)");
+				// Third Condition
+				System.out.println(i + " - DeepTech (also multiple of both 3 & 5)");
 			}
-			else {
+			if(i%3!=0 && i%5!=0) {
+				// Rest of Numbers
 				System.out.println(i);
 			}
-			
 		}
 
 	}
